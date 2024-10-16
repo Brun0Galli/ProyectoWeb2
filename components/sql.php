5629539ae230a3c19acdbfce20eb604e2824ea43
Bruno <0256993@up.edu.mx>
2024-10-16 17:52:58 -0600

    added files

--- components/sql.php
+++ components/sql.php
@@ -1,11 +1,21 @@
 <?php
-
 $servername = "localhost";
 $username = "pantera";
-$password = "panteracualquiera";
-$dbname = "panteras";
+$password = "cualquiera";
+$dbname = "panteras2";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "connected successfully <br> ";
+    $query = "select * from asesor";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["ID"] . "             " . " - Correo: " . $row["Correo"] . "              " . " - Nombre: "  . $row["Nombre"]  . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+}
--- index.php
+++ index.php
@@ -1,70 +1,296 @@
-
 <?php
+$servername = "localhost";
+$username = "pantera";
+$password = "cualquiera";
+$dbname = "panteras2";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    include 'components/sql.php';
-
-    $productos = [];
-    
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="style.css" rel="stylesheet">
-        <script src="script.js"></script>
-    </head>
-    <body>
-        <div class="container vh-10 vw-100">
-            <div class="d-flex justify-content-around my-4">
-                <div id="filters" class="filters-gap justify-content-left flex-column">
-                    <div id="row-1" class="filters-gap d-flex justify-content-left">
-                        <div class="filter-box box-med">
-                            <span class="t-accent">Sede:</span>
-                            <span>Fecha</span>
-                        </div>
-                    </div>
-                    <div id="row-2" class="filters-gap d-flex justify-content-left">
-                        <div class="filter-box box-std">
-                            <span class="t-accent">Inicio:</span>
-                            <span class="">Fecha</span>
-                        </div>
-                        <div class="btn button-box calendar-box"><img src="calendar-icon.png" class="icon"></div>
-                        <div class="filter-box box-std">
-                            <span class="t-accent">Fin:</span>
-                            <span>Fecha</span>
-                        </div>
-                        <div class="btn button-box calendar-box"><img src="calendar-icon.png" class="icon"></div>
-                    </div>
-                    <div id="row-3" class="filters-gap d-flex justify-content-left">
-                        <div class="filter-box box-med">
-                            <span class="t-accent">Talent:</span>
-                            <span>Fecha</span>
-                        </div>
-                        <div class="filter-box box-med">
-                            <span class="t-accent">Categorias:</span>
-                            <span>Fecha</span>
-                        </div>
-                    </div>
-                </div>
-                <div id="buttons" class="d-flex justify-content-right flex-column">
-                    <button class="btn button-box button-std" onclick="limpiar()">
-                        <span>Limpiar</span>
-                        <img src="search-icon.png" class="icon">
-                    </button>
-                    <button class="btn button-box bg-accent button-std" onclick="buscar()">
-                        <img src="trash-icon.png" class="icon">
-                        <span>Trash</span>
-                    </button>
-                </div>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asesorías Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+    <style>
+        body {
+            background-color: #1e1e1e;
+            color: #fff;
+        }
+
+        .input-group {
+            width: auto;
+        }
+
+        .input-group-text-custom {
+            background-color: #333;
+            color: #807834;
+            border-radius: 20px 0 0 20px;
+            border: none;
+            padding: 0.375rem 0.75rem;
+        }
+
+        .form-select-custom {
+            background-color: #333;
+            color: #6c757d;
+            border: none;
+            border-radius: 20px;
+        }
+
+        .input-custom {
+            background-color: #333;
+            color: white;
+            border: none;
+            border-radius: 20px;
+        }
+
+        .btn-custom {
+            background-color: #6c757d;
+            border: none;
+            border-radius: 10px;
+            padding: 0.375rem 0.75rem;
+            color: white;
+        }
+
+        .btn-custom2 {
+            background-color: #635e29;
+            border: none;
+            border-radius: 10px;
+            padding: 0.375rem 0.75rem;
+            color: white;
+        }
+
+        .btn-custom i {
+            font-size: 1.2rem;
+        }
+
+        .daterangepicker {
+            background-color: #333;
+            /* Fondo oscuro */
+            color: white;
+            /* Texto blanco */
+        }
+
+        .daterangepicker .calendar-table {
+            color: white;
+        }
+
+        .daterangepicker td.active,
+        .daterangepicker td.active:hover {
+            background-color: #635e29;
+            /* Color del fondo activo */
+            color: white;
+        }
+
+        .daterangepicker td.start-date,
+        .daterangepicker td.end-date {
+            background-color: #635e29;
+            color: white;
+        }
+
+        .daterangepicker td.available:hover {
+            background-color: #6c757d;
+            /* Color del hover */
+            color: white;
+        }
+
+        .daterangepicker .applyBtn,
+        .daterangepicker .cancelBtn {
+            background-color: #635e29;
+            /* Botones aplicar/cancelar */
+            border: none;
+        }
+
+        .daterangepicker .applyBtn:hover,
+        .daterangepicker .cancelBtn:hover {
+            background-color: #6c757d;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="d-flex">
+                <button class="btn btn-custom mx-2">
+                    <i class="bi bi-file-minus-fill"></i>
+                </button>
+                <h4>Filtros</h4>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    </body>
+        <hr>
+        <form class="row" id="filterForm">
+            <div class="row">
+                <div class="col-3">
+                    <div class="input-group" style="max-width: 400px;">
+                        <span class="input-group-text input-group-text-custom">Sede:</span>
+                        <select class="form-select form-select-custom" id="sede">
+                            <option selected>Todas las sedes</option>
+                            <option value="1">Categoría 1</option>
+                            <option value="2">Categoría 2</option>
+                            <option value="3">Categoría 3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="row m-auto mt-3">
+                    <div class="col-md-10">
+                        <div class="d-flex align-items-center">
+                            <div class="input-group" style="max-width: 400px;">
+                                <span class="input-group-text input-group-text-custom">Inicio:</span>
+                                <input type="text" class="input-custom me-2" id="fechaInicio">
+                            </div>
+                            <button class="btn btn-custom mx-2" type="button" id="calendarioInicio">
+                                <i class="bi bi-calendar-x-fill"></i>
+                            </button>
+                            <div class="input-group" style="max-width: 400px;">
+                                <span class="input-group-text input-group-text-custom ms-2">Fin:</span>
+                                <input type="text" class="input-custom me-2" id="fechaFin">
+                            </div>
+                            <button class="btn btn-custom mx-2" type="button" id="calendarioFin">
+                                <i class="bi bi-calendar-x-fill"></i>
+                            </button>
+                            <div class="input-group" style="max-width: 400px;">
+                                <span class="input-group-text input-group-text-custom ms-2">Talent:</span>
+                                <select class="form-select form-select-custom" id="talent">
+                                    <option selected>Seleccione un miembro</option>
+                                    <!-- Aquí cargarás los profesores desde la base de datos -->
+                                    <?php
+                                    $query = "select * from asesor";
+                                    $result = $conn->query($query);
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_assoc()) {
+                                            echo "<option value='" . $row["ID"] . "'>" . $row["Nombre"] . "</option>";
+                                        }
+                                    } else {
+                                        echo "<option value='0'>No hay profesores</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col m-auto">
+                        <button type="reset" class="btn btn-lg rounded-pill btn-custom me-3" id="limpiarCampos">
+                            Limpiar<i class="bi bi-trash ms-2"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="row m-auto mt-3">
+                    <div class="col-md-10">
+                        <div class="d-flex">
+                            <div class="input-group" style="max-width: 400px;">
+                                <span class="input-group-text input-group-text-custom">Categorías:</span>
+                                <select class="form-select form-select-custom" id="categoria">
+                                    <option selected>Seleccione una Categoría</option>
+                                    <?php
+                                    $query = "select * from categoria";
+                                    $result = $conn->query($query);
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_assoc()) {
+                                            echo "<option value='" . $row["ID"] . "'>" . $row["Nombre"] . "</option>";
+                                        }
+                                    } else {
+                                        echo "<option value='0'>No hay categorías</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-lg rounded-pill btn-custom2" id="buscar">
+                            <i class="bi bi-search me-3"></i>Buscar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- Scripts -->
+    <script>
+        $(document).ready(function() {
+            // Inicializar el calendario
+            $('#calendarioInicio').on('click', function() {
+                $('#fechaInicio').daterangepicker({
+                    singleDatePicker: true,
+                    timePicker: true,
+                    timePicker24Hour: true,
+                    showDropdowns: true, // Agregar dropdowns para seleccionar el año y el mes
+                    locale: {
+                        format: 'YYYY-MM-DD HH:mm',
+                        applyLabel: "Aplicar",
+                        cancelLabel: "Cancelar",
+                        daysOfWeek: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+                        monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+                        firstDay: 1 // Comienza la semana en lunes
+                    }
+                });
+            });
+
+            $('#calendarioFin').on('click', function() {
+                $('#fechaFin').daterangepicker({
+                    singleDatePicker: true,
+                    timePicker: true,
+                    timePicker24Hour: true,
+                    showDropdowns: true, // Agregar dropdowns para seleccionar el año y el mes
+                    locale: {
+                        format: 'YYYY-MM-DD HH:mm',
+                        applyLabel: "Aplicar",
+                        cancelLabel: "Cancelar",
+                        daysOfWeek: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+                        monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+                        firstDay: 1 // Comienza la semana en lunes
+                    }
+                });
+            });
+
+            // Buscar
+            $('#buscar').on('click', function(e) {
+                e.preventDefault();
+                // Aquí ejecutas el query con AJAX
+                var sede = $('#sede').val();
+                var inicio = $('#fechaInicio').val();
+                var fin = $('#fechaFin').val();
+                var talent = $('#talent').val();
+                var categoria = $('#categoria').val();
+
+                $.ajax({
+                    url: 'buscar.php', // Aquí pones el archivo PHP que ejecuta el query
+                    method: 'POST',
+                    data: {
+                        sede: sede,
+                        inicio: inicio,
+                        fin: fin,
+                        talent: talent,
+                        categoria: categoria
+                    },
+                    success: function(response) {
+                        // Aquí muestras los resultados
+                    }
+                });
+            });
+
+            // Limpiar
+            $('#limpiarCampos').on('click', function() {
+                $('#filterForm')[0].reset(); // Resetea el formulario
+            });
+        });
+    </script>
+</body>
+
 </html>
